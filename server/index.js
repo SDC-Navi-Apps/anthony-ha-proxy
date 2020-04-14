32a49ef1c2dea8b2815853addd296ccc7d570a61
brice-klein <bcklein3@gmail.com>
2020-04-14 17:44:18 -0400

    installed new-relic

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
+require('newrelic');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
+
 
 app.use(express.static(__dirname + '/../client'));
 
